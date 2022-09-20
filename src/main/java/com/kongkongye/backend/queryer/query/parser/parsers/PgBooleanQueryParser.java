869ce87ca20c12d737209c1ad4f5b643b64f293c
kongkongye <kongkongye@163.com>
2022-09-20 12:53:07 +0800

    1

---
+++ src/main/java/com/kongkongye/backend/queryer/query/parser/parsers/PgBooleanQueryParser.java
@@ -0,0 +1,27 @@
+package com.kongkongye.backend.queryer.query.parser.parsers;
+
+import com.kongkongye.backend.queryer.SqlHelper;
+import com.kongkongye.backend.queryer.common.ReflectionUtil;
+import com.kongkongye.backend.queryer.query.parser.QueryParser;
+
+import java.lang.reflect.Field;
+import java.util.Map;
+
+/**
+ * null与0代表false
+ * 1代表true
+ */
+public class PgBooleanQueryParser implements QueryParser {
+    @Override
+    public void parse(StringBuilder selSql, SqlHelper.FromBuilder fromSql, StringBuilder whereSql, StringBuilder groupSql, Map<String, Object> params,
+                      Object obj, String alias, Field field, String fieldName, String sqlFieldName) {
+        Boolean value = (Boolean) ReflectionUtil.getField(field, obj);
+        if (value != null) {
+            if (value) {
+                whereSql.append(" and ").append(alias).append(".").append(sqlFieldName).append(" = true ");
+            } else {
+                whereSql.append(" and (").append(alias).append(".").append(sqlFieldName).append(" = false or ").append(alias).append(".").append(sqlFieldName).append(" is null) ");
+            }
+        }
+    }
+}
